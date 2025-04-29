ae1af10f17038bc93d1a61ed16ab587418640bab
AiRok386 <rahulthakur432001@gmail.com>
2025-04-30 00:03:32 +0530

    Update transaction.model.js

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -11,8 +11,7 @@ const TransactionSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Transaction', TransactionSchema);
-const mongoose = require('mongoose');
+
 
 const transactionSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
