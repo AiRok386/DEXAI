921dbe519513f005602dae2321b98ab6ce985473
AiRok386 <rahulthakur432001@gmail.com>
2025-05-01 17:59:41 +0530

    Update trade.routes.js

--- routes/trade.routes.js
+++ routes/trade.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const tradingController = require('../controllers/trade.controller');
-const { protectUser } = require('../middlewares/auth.middleware');
+const { protect } = require('../middlewares/auth.middleware');
 
 // ⭐ Place Order
-router.post('/order', protectUser, tradingController.placeOrder);
+router.post('/order', protect, tradingController.placeOrder);
 
 // ⭐ Fetch Order Book
 router.get('/orderbook', tradingController.getOrderBook);
