da4ba395b97324e2bbfd11ff84bf567115242768
AiRok386 <rahulthakur432001@gmail.com>
2025-05-02 17:29:20 +0530

    Update updateTokenPrices.js

--- services/updateTokenPrices.js
+++ services/updateTokenPrices.js
@@ -1,4 +1,6 @@
-const COINCAP_API_KEY = process.env.https://rest.coincap.io/v3/assets?apiKey=6ba0e905909d943ad2aca41ffcd74346948025973e52a82c670373f38eccbabe;
+require('dotenv').config(); // Make sure this is at the top if not already present
+
+const COINCAP_API_KEY = process.env.COINCAP_API_KEY;
 
 const response = await fetch(`https://rest.coincap.io/v3/assets/${token.assetId}`, {
   headers: {
