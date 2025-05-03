6178032b1641b81f5440fc142993bc20a9e03287
AiRok386 <rahulthakur432001@gmail.com>
2025-05-03 23:16:41 +0530

    Create constants.js

---
+++ config/constants.js
@@ -0,0 +1,21 @@
+// config/constants.js
+
+module.exports = {
+  topPairs: [
+    'BTCUSDT',
+    'ETHUSDT',
+    'BNBUSDT',
+    'SOLUSDT',
+    'XRPUSDT',
+    'DOGEUSDT',
+    'PEPEUSDT',
+    'SUIUSDT',
+    'ADAUSDT',
+    'TRXUSDT',
+    'TONUSDT',
+    'SHIBUSDT',
+    'AVAXUSDT',
+    'LINKUSDT',
+    'DOTUSDT'
+  ]
+};
