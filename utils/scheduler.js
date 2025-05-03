14f3b33304b7aaab846c15ede1f0c96ab0e00463
AiRok386 <rahulthakur432001@gmail.com>
2025-05-03 19:37:38 +0530

    Update scheduler.js

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -1,34 +1,17 @@
-const cron = require('node-cron');
-const { fetchMarketData } = require('../services/coinCapService');
-const { saveOrUpdateMarketData } = require('../controllers/marketController');
+// utils/scheduler.js
 
-// Async function to start the scheduler
-async function startScheduler() {
-  // Schedule a cron job to fetch data every minute
-  cron.schedule('* * * * *', async () => {
-    try {
-      console.log('[Scheduler] Fetching market data from CoinCap...');
-      
-      // Fetch market data
-      const data = await fetchMarketData();
+const { fetchAndStoreMarketData } = require('./binanceMarketFetcher');
 
-      if (Array.isArray(data) && data.length > 0) {
-        // Loop through all assets and save them to the database
-        for (const asset of data) {
-          await saveOrUpdateMarketData(asset); // Assuming this is an async function
-        }
-        console.log(`[Scheduler] Synced ${data.length} assets from CoinCap`);
-      } else {
-        console.warn('[Scheduler] No asset data received');
-      }
-    } catch (err) {
-      console.error('[Scheduler] Error fetching market data:', err.message);
-    }
-  });
+const INTERVAL_MS = 5000; // Every 5 seconds
 
-  console.log('[Scheduler] Token price updater started and running every minute.');
+function startScheduler() {
+  console.log('🕒 Binance market data fetcher running every 5 seconds...');
+  
+  // Run once immediately on startup
+  fetchAndStoreMarketData();
+
+  // Schedule repeated fetching
+  setInterval(fetchAndStoreMarketData, INTERVAL_MS);
 }
 
-module.exports = {
-  startScheduler
-};
+module.exports = { startScheduler };
