89778c52c6581cb2d657f9d1f810272a4a1ad1ee
AiRok386 <rahulthakur432001@gmail.com>
2025-05-03 22:12:51 +0530

    Update market.routes.js

--- routes/market.routes.js
+++ routes/market.routes.js
@@ -7,4 +7,12 @@ router.get('/top15', (req, res) => {
   res.json(getMemoryData());
 });
 
+const { getAllMarketData, getMarketData } = require('../marketDataStore');
+
+router.get('/markets/:symbol', (req, res) => {
+  const data = getMarketData(req.params.symbol.toUpperCase());
+  if (!data) return res.status(404).json({ error: 'Symbol not found' });
+  res.json(data);
+});
+
 module.exports = router;
