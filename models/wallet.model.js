494368861a851545616f9e70aa8270a021e0864b
AiRok386 <rahulthakur432001@gmail.com>
2025-05-03 16:48:07 +0530

    Update wallet.model.js

--- models/wallet.model.js
+++ models/wallet.model.js
@@ -1,16 +1,33 @@
 const mongoose = require('mongoose');
 
 // ⭐ Wallet schema
-const walletschema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    currency: { type: String, required: true },
-    balance: { type: Number, default: 0 },
-    address: { type: String },
-
+const walletSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true
+    },
+    balance: {
+        type: Number,
+        default: 0
+    },
+    address: {
+        type: String
+    },
     // ⭐ For each token/coin
     balances: [{
-        tokenSymbol: { type: String, required: true }, // like BTC, ETH, USDT
-        balance: { type: Number, default: 0 }
+        tokenSymbol: {
+            type: String,
+            required: true
+        }, // like BTC, ETH, USDT
+        balance: {
+            type: Number,
+            default: 0
+        }
     }]
 }, { timestamps: true });
 
