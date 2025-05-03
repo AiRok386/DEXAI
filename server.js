7fc42df66c4a0ff440fec3df3cf5fd0a3f9996c7
AiRok386 <rahulthakur432001@gmail.com>
2025-05-03 23:34:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -94,5 +94,15 @@ app.use('/api/orderbook', orderbookRoutes);
 const tradesRoutes = require('./routes/trades.routes');
 app.use('/api/trades', tradesRoutes);
 const { startUpdater } = require('./Jobs/dataUpdater');
+const rateLimit = require('express-rate-limit');
+
+const apiLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.'
+});
+
+app.use('/api/', apiLimiter); // Apply rate-limiting to all API routes
+
 startUpdater();
 
